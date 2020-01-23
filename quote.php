0c80250302679ebe71f2342145a42e5f468c630b
NelsonTinega <38856299+NelsonTinega@users.noreply.github.com>
2020-01-23 12:37:39 +0300

    Update quote.php

--- quote.php
+++ quote.php
@@ -8,7 +8,7 @@ include("functions/functions.php");
 
 <head> 
 
- <title>Instant Movers Official website</title>
+ <title>Movers website</title>
    <meta charset="UTF-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
