203f53027a72c5b1f950931c8a0a5e701e0ec3a9
NelsonTinega <38856299+NelsonTinega@users.noreply.github.com>
2020-01-23 12:25:14 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE>
+<?php 
+include("functions/functions.php");
+?>
+<html lang="en us">
+
+<head> 
+
+ <title>Instant Movers Official website</title>
+   <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="instant movers is a company speciliazed in quick and efficient relocation of its clients' belongings. Lets move together">
+	<meta name="keywords" content="movers,moving, Home relocation,office relocation,mover">
+   
+   
+    <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+   <script src="bootstrap/js/respond.js"></script>
+  
+  <link rel="stylesheet" href="styles/styles.css" media="all"/>
+  
+   <link rel="stylesheet" type="text/css" href="engine1/style.css" />
+	<script type="text/javascript" src="engine1/jquery.js"></script>
+</head>
+<body>
+<div class="coLogo">
+<img id="mainlogo" src="pictures/logo.png"/>
+</div>
+
+
+<div class="menubar">
+<ul id="menu">
+   <li><a href="index.php" />Home</li>
+   <li><a href="contacts.php" />Contact us</li>
+   <li><a href="quote.php" />Request Quote</li>
+   <li><a href="testimonials.php" />Testimonials</li>
+    <li><a href="services.php" />Our Services</li>
+
+</ul>
+
+</div>
+</div>
+ 
+<div class="content_wrapper">
+
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/2.png" alt="2" title="2" id="wows1_0"/></li>
+		<li><img src="data1/images/3.jpg" alt="3" title="3" id="wows1_1"/></li>
+		<li><img src="data1/images/4.jpg" alt="4" title="4" id="wows1_2"/></li>
+		<li><img src="data1/images/5.jpg" alt="5" title="5" id="wows1_3"/></li>
+		<li><img src="data1/images/6.jpg" alt="6" title="6" id="wows1_4"/></li>
+		<li><img src="data1/images/7.jpg" alt="7" title="7" id="wows1_5"/></li>
+		<li><img src="data1/images/8.png" alt="8" title="8" id="wows1_6"/></li>
+		<li><img src="data1/images/9.jpg" alt="9" title="9" id="wows1_7"/></li>
+		<li><a href="http://wowslider.net"><img src="data1/images/10.jpg" alt="jquery slider" title="10" id="wows1_8"/></a></li>
+		<li><img src="data1/images/11.png" alt="11" title="11" id="wows1_9"/></li>
+	</ul></div>
+<div class="ws_script" style="position:absolute;left:-99%"><a href="http://wowslider.com">slider</a> by WOWSlider.com v8.6</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<div class="main_wrapper">
+
+<div id="table1">
+<table style="margin:auto">
+<tr> <th>Contacts</th><th>About us</th><th>Why choose us</th></tr>
+<tr> <td>Email:instantmovers2012@gmail.com <br>Website: instantmovers.co.ke facebook page:instant movers</td><td>The company was started in 2012 so as to assist clients whenever they want to relocate </td><td>We are fast, proper handling of client's propery, safe transportation of the products</td></tr>
+</table>
+</div>
+ 
+<div id="footer"> 
+	 <h2 style="text-align:center;padding_top:50px;">&copy; 2017 by nelTechs</h2>
+	 
+	 </div>
+</div>
+</div>
+</body>
+
+</html>
